6126de1f20093c2ccc62f963771303b598a9bafb
yindd <695685009@qq.com>
2019-09-25 20:19:00 +0800

    单击按钮显示列表，再次点击隐藏列表

--- day2/category/category.js
+++ day2/category/category.js
@@ -1,6 +1,23 @@
 $(function () {
-    //查询大于第五条且并非最后一条的li列表对象
+    //查询大于第五条且并非最后一条的li列表对象隐藏
     var $category = $("ul li:gt(3):not(:last)");
-    $category.hide();
+
+    //获取显示全部品牌按钮
+    var $toggleBtn = $("div .showmore > a");
+    $toggleBtn.click(function () {
+        if($category.is(":visible"))
+        {
+            $category.hide();
+            //按钮显示精简品牌
+            $(".showmore a span").text("显示全部品牌");
+        }else{
+            $category.show();
+            $(".showmore a span").text("精简显示品牌");
+        }
+
+        return false;
+    });
+
+
     console.log(123);
-})
+});

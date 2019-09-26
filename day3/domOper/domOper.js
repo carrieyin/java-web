3fef0ba241c80b0955083104c9503680662cd889
yindd <695685009@qq.com>
2019-09-26 22:24:34 +0800

    dom 操作

--- day3/domOper/domOper.js
+++ day3/domOper/domOper.js
@@ -1,6 +1,9 @@
 $(function () {
-    var $tootip = $("a .tooltip");
-    if($tootip.mouseover()){
-        $(this)
-    }
+    var $tip = $("a .tooltip");
+    $tip.mouseover(function (e) {
+        var $tooltip = "<div id='tooltip'" + this.title + "</div>";
+        $("#tooltip")
+            .css({"top":e.pageY + "px",
+                        "left":e.pageX + "px"});
+    });
 })

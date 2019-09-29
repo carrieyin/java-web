89f7c77bfa87678451e1866120b0c04e9e360f48
yindd <695685009@qq.com>
2019-09-29 22:07:10 +0800

    测试dom操作

--- day3/domOper/domTest.js
+++ day3/domOper/domTest.js
@@ -1,13 +1,24 @@
 $(function () {
-    var $tip = $("a.tooltip");
-    $tip.mouseover(function (e) {
-        var tooltip = "<div id='tooltip'" + this.title + "</div>";
+        $("a.tooltip").mouseover(function (e) {
+        this.myTitle = this.title;
+        this.title = "";
+        console.log(this.myTitle);
+        var tooltip = "<div id='tooltip'>" + this.myTitle + "</div>";
         $("body").append(tooltip);
+        console.log("mouse over");
         $("#tooltip")
             .css({"top":e.pageY + "px",
                         "left":e.pageX + "px"})
             .show("fast");
-        console.log("test");
-        });
-    $tip.mousedown()
+        console.log(e.pageX );
+        }).mouseout(function(){
+            this.title = this.myTitle;
+        $("#tooltip").remove();
+        //console.log("mouse out");
+    }).mousemove(function (e) {
+        $("#tooltip")
+            .css("left",e.pageX).css("top",e.pageY);
+        //console.log(e.pageX);
+    });
+
 })

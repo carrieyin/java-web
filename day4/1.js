fef52476e1258b03a9aabdc61b8dd43cbadd943b
yindd <695685009@qq.com>
2019-10-13 16:12:08 +0800

    表单操作

--- day4/1.js
+++ day4/1.js
@@ -4,6 +4,7 @@ $(function () {
         $(this).addClass("focus");
     }).blur(function () {
         $(this).removeClass("focus");
+
     });
 
     var $msg = $("#msg");
@@ -18,16 +19,48 @@ $(function () {
             //$msg.height($msg.height() - 50);
             $msg.animate({height: "-=50"}, 400);
         }
-    })
+    });
 
     $(".up").click(function () {
         if( $msg.is(":animated")){
             $msg.animate({ scrollTop : "-50"}, 400)
         }
 
-    })
+    });
 
     $(".down").click(function () {
         $msg.animate({ scrollTop : "+50"}, 400)
-    })
+    });
+
+    $("#checkedAll").click(function () {
+        $("[name=item]:checkbox").attr("checked", "true");
+    });
+
+    $("#checkedNo").click(function () {
+        //$("[name=item]:checkbox").attr("checked", "false");
+        $("[name=item]:checkbox").removeAttr("checked");
+        //console.log($("[name=item]:checkbox").attr());
+    });
+
+    $("#send").click(function () {
+        var str = "你选中的是: \r\n";
+        $("[name=item]:checkbox:checked").each(function () {
+            str += $(this).val() + "\r\n";
+        });
+      alert(str);
+    });
+
+    $("#add").click(function () {
+        var $options = $("#select1 option:selected");
+        //$options.appendTo("#select2");
+        $("#select2").append($options);
+    });
+
+    $("form input.required").each(function () {
+        var $required = $("<strong class='high'>* </strong>");
+        console.log($required);
+        $(this).parent().append($required);
+    });
+
+
 })
